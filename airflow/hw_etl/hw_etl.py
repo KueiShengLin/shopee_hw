0c4aa62bf19b87cc2afb7b511a3f5947b4816e39
kslin <kslin@localhost.localdomain>
2022-04-16 18:53:24 +0800

    airflow remove missing data and enhance price cleaning

--- airflow/hw_etl/hw_etl.py
+++ airflow/hw_etl/hw_etl.py
@@ -5,6 +5,7 @@ from datetime import datetime
 def basic_data_clean(handle_date):
     """
     clean basic data
+      - remove missing data
       - change count values to int e.g. sold, comment_count 1.6萬 => 16000
       - process price value to float ex: $1,234 => 1234
     
@@ -23,11 +24,13 @@ def basic_data_clean(handle_date):
 
     date_str = handle_date.strftime("%Y-%m-%d")
     engine = create_engine("postgresql://postgres:123qwe@192.168.59.1:5432/crawler")
-    df = pd.read_sql(f"SELECT * FROM raw.basic WHERE parse_date::date = '{date_str}' ", con=engine)
+    df = pd.read_sql(f"SELECT * FROM raw.basic WHERE parse_date::date = '{date_str}' and name != '' and price != ''", con=engine)
 
     df["comment_count"] = _count_transfer(df["comment_count"])
     df["sold"] = _count_transfer(df["sold"])
-    df["price"] = df["price"].str.replace("$", "").str.replace(",", "").astype(float)
 
+    df["price"] = df["price"].str.replace("$", "").str.replace(",", "").str.split("-")
+    df["price"] = df["price"].apply(max).astype(float)
+    
     df.to_sql(schema="etl", name="basic", con=engine, index=False, if_exists="replace")
         
